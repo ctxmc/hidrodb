0f9e1fa8c3643e07a7f8ed170c3a545880107add
cortexmancer <git@thecortexmancer.org>
2026-06-29 13:49:50 -0300

    chore: test custom arguments

--- tests/test_config.py
+++ tests/test_config.py
@@ -16,3 +16,26 @@ def test_setup_arguments_defaults():
         assert hidrodb.jobs.BATCH_SIZE      == 1000
         assert hidrodb.database.CLIENT_PATH == 'db/client.db'
         assert hidrodb.database.HIDRO_PATH  == 'db/hidro.db'
+
+
+def test_setup_arguments_custum():
+    """Test default argument values"""
+
+    from unittest.mock import patch
+
+    test_arguments = ['script_name',
+                      '--log-level',   'DEBUG',
+                      '--max-workers', '5',
+                      '--batch-size',  '500',
+                      '--hidro',       'custom/hidro.db',
+                      '--client',      'custom/client.db']
+
+    with patch('sys.argv', test_arguments):
+        import hidrodb.jobs, hidrodb.database
+
+        hidrodb.config.setup_arguments()
+        assert hidrodb.config.LOG_LEVEL     == 'DEBUG'
+        assert hidrodb.jobs.MAX_WORKERS     == 5
+        assert hidrodb.jobs.BATCH_SIZE      == 500
+        assert hidrodb.database.CLIENT_PATH == 'custom/client.db'
+        assert hidrodb.database.HIDRO_PATH  == 'custom/hidro.db'
